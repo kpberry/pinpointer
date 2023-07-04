aa97d0600391aa7ef659d568b6ac39c5ccf3b324
kpberry <kpberry11@gmail.com>
2023-07-03 22:14:14 -0400

    Add documentation to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+//! # pinpointer
+//! This crate can be used to get provinces and countries from latitudes and longitudes.
+//!
+//! ## Usage
+//! The main feature of this library is the `LabeledPartitionTree`, which can be used to perform fast point-in-region queries. 
+//! A `LabeledPartitionTree` can be built from a mapping from labels to polygons with those labels.
+//!
+//! This library provides some helper functions to make it easy to get map data and build label trees to perform point-in-country and point-in-province queries.
+//! See the examples folder for full code examples for downloading data, computing the label trees, and finally performing millions of point-in-country/point-in-province lookups.
+
 pub mod datasets;
 pub mod labeling;
